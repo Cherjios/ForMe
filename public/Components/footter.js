0e3a16499ba96535515a6d7a083077884f030363
cherjios <slopez0286@gmail.com>
2020-08-30 14:22:23 -0700

    Adding footer component to signup

---
+++ public/Components/footter.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+
+
+    return e(
+        "footer", null, React.createElement("div", {
+            className: "container-fluid sfooter"
+          }, React.createElement("div", {
+            className: "row"
+          }, React.createElement("div", {
+            className: "col"
+          }, React.createElement("h3", {
+            className: " text-center"
+          }, "Sergio Lopez"))), React.createElement("div", {
+            className: "container text-center"
+          }, React.createElement("div", {
+            className: "row"
+          }, React.createElement("div", {
+            className: "col-md-4"
+          }, React.createElement("a", {
+            className: "m-4",
+            href: "mailto:slopez0286@gmail.com?subject=Mail-frommy-Website",
+            target: "_blank"
+          }, React.createElement("img", {
+            src: "https://img.icons8.com/material-rounded/48/000000/important-mail.png"
+          }))), React.createElement("div", {
+            className: "col-md-4"
+          }, React.createElement("a", {
+            className: "m-4",
+            href: "https://github.com/Cherjios",
+            target: "_blank"
+          }, React.createElement("img", {
+            src: "https://img.icons8.com/material-sharp/48/000000/github.png"
+          }))), React.createElement("div", {
+            className: "col-md-4"
+          }, React.createElement("a", {
+            className: "m-4",
+            href: "https://www.linkedin.com/in/sergio-lopez-81790579/",
+            target: "_blank"
+          }, React.createElement("img", {
+            src: "https://img.icons8.com/ios-filled/50/000000/linkedin.png"
+          })))), React.createElement("div", {
+            className: "row"
+          }, React.createElement("div", {
+            className: "col"
+          }, React.createElement("h4", null, React.createElement("a", {
+            className: "repoLink",
+            href: "https://github.com/Cherjios/Sign-my-form"
+          }, "Respo Link"))))))
+      
+    );
+  }
+}
+
+const domContainer = document.querySelector('#FooterHere');
+ReactDOM.render(e(LikeButton), domContainer);
