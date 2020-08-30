6f9c9197c7e0876e451933bd711e1a06bae2b3ea
cherjios <slopez0286@gmail.com>
2020-08-30 16:06:15 -0700

    Adding navbar as a componenet on memebers

--- public/Components/navbar1.js
+++ public/Components/navbar1.js
@@ -14,40 +14,32 @@ class Navbar1 extends React.Component {
     return e(
 
       "nav", {
-      className: "navbar navbar-light navbar-expand-md bg-faded justify-content-center"
-    }, React.createElement("a", {
-      href: "/members",
-      className: "navbar-brand d-flex w-50 mr-auto"
-    }, React.createElement("samp", {
-      className: "title-font"
-    }, "ForMe")), React.createElement("button", {
-      className: "navbar-toggler",
-      type: "button",
-      "data-toggle": "collapse",
-      "data-target": "#collapsingNavbar3"
-    }, React.createElement("span", {
-      className: "navbar-toggler-icon"
-    })), React.createElement("div", {
-      className: "navbar-collapse collapse w-100",
-      id: "collapsingNavbar3"
-    }, React.createElement("ul", {
-      className: "nav navbar-nav ml-auto w-100 justify-content-end"
-    }, React.createElement("li", {
-      className: "nav-item"
-    }, React.createElement("a", {
-      className: "nav-link ",
-      href: "/members"
-    }, React.createElement("span", {
-      className: ""
-    }, "Home"))), React.createElement("li", {
-      className: "nav-item text-white mtext"
-    }, React.createElement("a", {
-      className: "nav-link",
-      href: "/logout"
-    }, React.createElement("span", {
-      className: ""
-    }, "Logout")))))
-    );
+        class: "navbar navbar-light navbar-expand-md bg-faded justify-content-center navcolor1"
+      }, React.createElement("a", {
+        href: "/members",
+        class: "navbar-brand d-flex w-50 mr-auto"
+      }, React.createElement("samp", {
+        class: "text-white textNav"
+      }, "ForMe")), React.createElement("button", {
+        class: "navbar-toggler",
+        type: "button",
+        "data-toggle": "collapse",
+        "data-target": "#collapsingNavbar3"
+      }, React.createElement("span", {
+        class: "navbar-toggler-icon"
+      })), React.createElement("div", {
+        class: "navbar-collapse collapse w-100",
+        id: "collapsingNavbar3"
+      }, React.createElement("ul", {
+        class: "nav navbar-nav ml-auto w-100 justify-content-end"
+      }, React.createElement("li", {
+        class: "nav-item"
+      }, React.createElement("a", {
+        class: "nav-link ",
+        href: "/logout"
+      }, React.createElement("span", {
+        class: "text-white"
+      }, "Logout"))))));
   }
 }
 
