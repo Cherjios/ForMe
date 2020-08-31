1b7d4fa935399aefaa0622c4b615ddab8e4f9ae7
cherjios <slopez0286@gmail.com>
2020-08-30 17:18:24 -0700

    givin style to footer

--- public/Components/footter.js
+++ public/Components/footter.js
@@ -8,53 +8,50 @@ class Footer extends React.Component {
   render() {
 
 
-    return React.createElement(
-        "footer", null, React.createElement("div", {
-            className: "container-fluid sfooter"
-          }, React.createElement("div", {
-            className: "row"
-          }, React.createElement("div", {
-            className: "col"
-          }, React.createElement("h3", {
-            className: " text-center"
-          }, "Sergio Lopez"))), React.createElement("div", {
-            className: "container text-center"
-          }, React.createElement("div", {
-            className: "row"
-          }, React.createElement("div", {
-            className: "col-md-4"
-          }, React.createElement("a", {
-            className: "m-4",
-            href: "mailto:slopez0286@gmail.com?subject=Mail-frommy-Website",
-            target: "_blank"
-          }, React.createElement("img", {
-            src: "https://img.icons8.com/material-rounded/48/000000/important-mail.png"
-          }))), React.createElement("div", {
-            className: "col-md-4"
-          }, React.createElement("a", {
-            className: "m-4",
-            href: "https://github.com/Cherjios",
-            target: "_blank"
-          }, React.createElement("img", {
-            src: "https://img.icons8.com/material-sharp/48/000000/github.png"
-          }))), React.createElement("div", {
-            className: "col-md-4"
-          }, React.createElement("a", {
-            className: "m-4",
-            href: "https://www.linkedin.com/in/sergio-lopez-81790579/",
-            target: "_blank"
-          }, React.createElement("img", {
-            src: "https://img.icons8.com/ios-filled/50/000000/linkedin.png"
-          })))), React.createElement("div", {
-            className: "row"
-          }, React.createElement("div", {
-            className: "col"
-          }, React.createElement("h4", null, React.createElement("a", {
-            className: "repoLink",
-            href: "https://github.com/Cherjios/Sign-my-form"
-          }, "Respo Link"))))))
-      
-    );
+    return React.createElement("footer", null, React.createElement("div", {
+      class: "container-fluid sfooter"
+    }, React.createElement("div", {
+      class: "row"
+    }, React.createElement("div", {
+      class: "col"
+    }, React.createElement("h3", {
+      class: " text-center"
+    }, "Sergio Lopez"))), React.createElement("div", {
+      class: "container text-center"
+    }, React.createElement("div", {
+      class: "row"
+    }, React.createElement("div", {
+      class: "col-md-4 col-sm-4"
+    }, React.createElement("a", {
+      class: "m-4",
+      href: "mailto:slopez0286@gmail.com?subject=Mail-frommy-Website",
+      target: "_blank"
+    }, React.createElement("img", {
+      src: "https://img.icons8.com/material-rounded/48/000000/important-mail.png"
+    }))), React.createElement("div", {
+      class: "col-md-4 col-sm-4"
+    }, React.createElement("a", {
+      class: "m-4",
+      href: "https://github.com/Cherjios",
+      target: "_blank"
+    }, React.createElement("img", {
+      src: "https://img.icons8.com/material-sharp/48/000000/github.png"
+    }))), React.createElement("div", {
+      class: "col-md-4 col-sm-4"
+    }, React.createElement("a", {
+      class: "m-4",
+      href: "https://www.linkedin.com/in/sergio-lopez-81790579/",
+      target: "_blank"
+    }, React.createElement("img", {
+      src: "https://img.icons8.com/ios-filled/50/000000/linkedin.png"
+    })))), React.createElement("br", null), React.createElement("div", {
+      class: "row"
+    }, React.createElement("div", {
+      class: "col"
+    }, React.createElement("h4", null, React.createElement("a", {
+      class: "repoLink",
+      href: "https://github.com/Cherjios/Sign-my-form"
+    }, "Respo Link")))))));
   }
 }
 
